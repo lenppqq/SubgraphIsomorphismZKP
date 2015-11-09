96cff1baf7b6d3f8de7f86a0260ff2a6954048fa
Peng Liang <lenppqq@163.com>
2015-11-09 14:07:28 -0500

    no message

---
+++ src/Graph.java
@@ -0,0 +1,19 @@
+
+/**
+ * This class is used to represent a graph
+ * @author Peng Liang (liangp@purdue.edu)
+ *
+ */
+public class Graph {
+	
+	public int[][] g;
+	
+	/**
+	 * Constructor
+	 * @param m number of rows of the graph
+	 * @param n number of columns of the graph
+	 */
+	public Graph(int m, int n) {
+	g = new int[m][n];
+	}
+}
