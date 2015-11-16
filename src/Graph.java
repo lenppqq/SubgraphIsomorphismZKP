f74b2480c58a728797e70f60bae55be3eec80a27
Peng Liang <liangp@purdue.edu>
2015-11-16 01:55:58 -0500

    deserialization

--- src/Graph.java
+++ src/Graph.java
@@ -1,37 +1,27 @@
-
-/**
- * This class is used to represent a graph
- * @author Peng Liang (liangp@purdue.edu)
- *
- */
 public class Graph {
-	
+
 	public int[][] g;
-	public int m;
 	public int n;
-	
-	/**
-	 * Constructor
-	 * @param m number of rows of the graph
-	 * @param n number of columns of the graph
-	 */
-	public Graph(int m, int n) {
-		this.m = m;
+
+	public Graph(int n) {
 		this.n = n;
-		g = new int[m][n];
-		for (int i = 0; i < m; i++) {
+		g = new int[n][n];
+		for (int i = 0; i < n; i++) {
 			for (int j = 0; j < n; j++) {
 				g[i][j] = 0;
 			}
 		}
 	}
+
+	public Graph(String s) throws Exception {
+		deserialize(s);
+	}
+
 	public String serialize() {
 		StringBuilder s = new StringBuilder();
-		s.append(m);
-		s.append(" ");
 		s.append(n);
 		s.append(" ");
-		for (int i = 0; i < m; i++) {
+		for (int i = 0; i < n; i++) {
 			for (int j = 0; j < n; j++) {
 				s.append(g[i][j]);
 				s.append(" ");
@@ -39,9 +29,24 @@ public class Graph {
 		}
 		return s.toString();
 	}
-	
+
+	public void deserialize(String s) throws Exception {
+		String[] items = s.split(" ");
+		int n = Integer.parseInt(items[0]);
+		if (items.length != n * n + 1) {
+			throw new Exception("Invalid input for deseriablization!");
+		}
+		g = new int[n][n];
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				g[i][j] = Integer.parseInt(items[n * i + j + 1]);
+			}
+		}
+		this.n = n;
+	}
+
 	public Graph getCommitment(int v) {
 		return null;
-		
+
 	}
 }
